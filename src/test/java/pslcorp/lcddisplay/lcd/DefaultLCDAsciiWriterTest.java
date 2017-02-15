789caa246166d1813a8415395434ee577ddbb569
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2017-02-14 22:28:37 -0500

    Refactor

--- src/test/java/pslcorp/lcddisplay/lcd/DefaultLCDAsciiWriterTest.java
+++ src/test/java/pslcorp/lcddisplay/lcd/DefaultLCDAsciiWriterTest.java
@@ -20,10 +20,22 @@ public class DefaultLCDAsciiWriterTest {
     }
 
     @Test
+    public void createAsciiZero(){
+
+    }
+
+    @Test
     public void createAsciiWithFewDigitsAndSmallSize() {
         String result = asciiWriter.createASCII(new UserInput((short) 2, "1234"));
         assertThat(result, equalTo
-                ("  ----  \n | | |||\n | | |||\n  ------\n ||  | |\n ||  | |\n  ----  \n"));
+                ("  ----  "+
+                "\n | | |||"+
+                "\n | | |||"+
+                "\n  ------"+
+                "\n ||  | |"+
+                "\n ||  | |"+
+                "\n  ----  \n"));
     }
+
 
 }
